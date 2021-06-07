ef5d18590b2979e57a53d5380ed1a060fd6edab9
nazish jahan <nazishjahan04@yahoo.com>
2021-06-07 13:10:01 +0500

    Initial Commit

---
+++ DestinationGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DestinationGH
+//
+//  Created by NAZISH ZOHAIB on 07/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Destination")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DestinationGH/DestinationGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestinationGHApp.swift
+//  DestinationGH
+//
+//  Created by NAZISH ZOHAIB on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DestinationGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
